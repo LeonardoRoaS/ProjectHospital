266329427b4d260b4ed78089f9de82b98d0b8914
Leonardo Roa <l.roa01@ufromail.cl>
2022-11-07 16:30:38 -0300

    Agregando y probando parametros al Launcher.

--- src/main/java/ProjectHospital/Launcher.java
+++ src/main/java/ProjectHospital/Launcher.java
@@ -8,6 +8,18 @@ public class Launcher {
 	}
 
 	public void Inicializar() {
+		Hospital hospital = new Hospital();
+		hospital.RegistroPaciente("Eduardo", 20);
+		hospital.ContratarMedico("Leonardo", 19);
+		hospital.ContratarMedico("Miguel", 39);
+		hospital.DespedirMedico("Leonardo");
+		System.out.println("=== MEDICOS ===");
+		hospital.MostrarMedicos();
+		System.out.println("=== PACIENTES ===");
+		hospital.MostrarPacientes();
+
+
+
 	}
 
 }
